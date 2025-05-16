f70506b13d1ce09fc365ad77a13b0a38bb4fbd8b
devashishmudigonda <94894150+devashishmudigonda@users.noreply.github.com>
2025-05-16 15:21:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,11 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Required for GitHub Pages (no Image Optimization server)
+    domains: [],
+    unoptimized: true,
   },
-  output: 'export', // Enables static HTML export
-  basePath: '/Devashish_Portfolio', // Required for GitHub Pages hosted under a subpath
-  assetPrefix: '/Devashish_Portfolio/', // Ensures CSS/JS loads correctly
 };
 
 module.exports = nextConfig;
